1f23598adde839ecb314555c67b7c1d4dd321968
Manak Raj <7manakraj@gmail.com>
2026-01-11 21:32:30 +0530

    Update script.js

--- script.js
+++ script.js
@@ -490,7 +490,6 @@ function animateRefresh() {
     }, 300);
 }
 
-
 // Add some smooth scrolling
 document.querySelectorAll('a[href^="#"]').forEach(anchor => {
     anchor.addEventListener('click', function (e) {
